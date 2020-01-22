cafca1d9987d19cd42de39b3f367b7f0a74450e1
Casper Thule <au443759@d39645.st.client.au.dk>
2020-01-22 15:19:33 +0100

    Attempt to fix pom

--- test-program/src/main/java/maestrov2/testprogram/Test.java
+++ test-program/src/main/java/maestrov2/testprogram/Test.java
@@ -1,11 +1,12 @@
 
 
 package maestrov2.testprogram;
-
+import org.maestro.ast.*;
 
 public class Test {
-	public static void main(String args[]) {
-		System.out.println("hej");
+	public static void main(String args[]){
+
+
 	}
 }
 
